79f57f1e19035a68788f7d2d2558beefb32ce415
LegendIU <v.galkin@innopolis.university>
2024-06-29 21:32:58 +0300

    Homework3

---
+++ types.ts
@@ -0,0 +1,20 @@
+// types.ts
+
+export interface Comic {
+    month: string;
+    num: number;
+    link: string;
+    year: string;
+    news: string;
+    safe_title: string;
+    transcript: string;
+    alt: string;
+    img: string;
+    title: string;
+    day: string;
+}
+
+export interface ComicIdResponse {
+    id: number;
+}
+
